8cd7567409362f61f46d55f98d452b8cc0b187a1
Kaio Neves <148585720+KaioNevess@users.noreply.github.com>
2026-04-02 16:12:15 -0300

    Create webscraping.py

---
+++ webscraping.py
@@ -0,0 +1,12 @@
+url = 'https://tribunademinas.com.br/noticias/cidade'
+
+response = requests.get(url)
+
+if response.status_code == 200: html_content = response.text soup = BeautifulSoup(html_content, 'html.parser') div_noticia = soup.find_all('div', class_='col-sm-12')
+
+   for noticia in div_noticia:
+    manchete = noticia.find('h2')
+    if manchete:
+       print(manchete.text)
+else: print('Falha na requisição: Status Code', response.status_code)
+
